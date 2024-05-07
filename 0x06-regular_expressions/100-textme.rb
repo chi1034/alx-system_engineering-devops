61b834e0ae565c60890144ced893c096f828c9e2
chi1034 <odiigift12@gmail.com>
2024-05-07 13:51:59 -0700

    odii

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 #done by odii chinenye gift
 
-puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?) \[flags:(.*?)\]/).join
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
